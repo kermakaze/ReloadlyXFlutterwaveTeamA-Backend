d2bb281c7ff93b00e46441daa2f7a9f74df87389
Elvis <ukojielvis@gmail.com>
2021-09-16 18:12:09 +0000

    Added campaign routes and added auth middleware

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const AuthController = require("../controllers/AuthController");
-
+const CampaignController = require('../controllers/CampaignController')
+const Auth = require('../middleware/auth');
 
 router.get('/', (req, res)=> res.send('RedeemFundApi v1.0'))
 
@@ -9,5 +10,14 @@ router.get('/', (req, res)=> res.send('RedeemFundApi v1.0'))
 //Authentication Routes
 router.post('/login',AuthController.login);
 router.post('/register',AuthController.register);
+
+
+//Routes beneath this require authentication
+router.use(Auth.checkAuth)
+
+//Campaign routes
+router.route('/campaigns')
+    .post(CampaignController.addCampaign)
+    .get(CampaignController.getCampaigns)
 
 module.exports = router;
