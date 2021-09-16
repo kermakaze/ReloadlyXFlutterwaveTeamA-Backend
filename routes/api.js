b87fd89b7ea3586b77d773808ca94d4b5ef16a23
Elvis <ukojielvis@gmail.com>
2021-09-16 17:23:35 +0000

    Added base route

--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,12 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 
+
+router.get('/', (req, res)=> res.send('RedeemFundApi v1.0'))
+
+
+//Authentication Routes
 router.post('/login',AuthController.login);
 router.post('/register',AuthController.register);
+
+module.exports = router;
