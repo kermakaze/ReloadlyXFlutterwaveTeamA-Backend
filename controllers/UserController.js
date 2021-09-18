f7ed9e32120d8c04d6c1d5e33fe3e485fbb7f562
Elvis <ukojielvis@gmail.com>
2021-09-18 10:47:53 +0000

    Added firstName and lastName when getting profile

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,7 @@ const Beneficiary = require('../models/Beneficiary');
 module.exports = {
 
     async getProfile(req, res) {
-        let beneficiary = await Beneficiary.findById(req.params['id'], 'id name email');
+        let beneficiary = await Beneficiary.findById(req.params['id'], 'id firstName lastName email');
         res.send(beneficiary);
     },
 
