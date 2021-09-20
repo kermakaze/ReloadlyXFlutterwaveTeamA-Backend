09e1d7357957385d26cf5f295fa628313852784a
Elvis <ukojielvis@gmail.com>
2021-09-20 13:28:29 +0000

    Added fields to routes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,7 @@ const Beneficiary = require('../models/Beneficiary');
 module.exports = {
 
     async getProfile(req, res) {
-        let beneficiary = await Beneficiary.findById(req.params['id'], 'id firstName lastName email');
+        let beneficiary = await Beneficiary.findById(req.params['id'], '-passwordHash -emailVerified');
         res.send(beneficiary);
     },
 
