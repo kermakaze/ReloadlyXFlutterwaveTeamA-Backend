9f90a0fcda1e6a4e1ee55384cc577c06e661dce4
Elvis <ukojielvis@gmail.com>
2021-09-20 13:18:24 +0000

    Added create profile route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,5 +9,23 @@ module.exports = {
         res.send(beneficiary);
     },
 
+    async createProfile(req, res){
+        try {
+            await Beneficiary.findByIdAndUpdate(req.decoded.id, {
+                profilePhotoS3: req.body['profilePhotoS3'],
+                countryCode: req.body['countryCode'],
+                countryName: req.body['countryName'],
+                phoneNumber: req.body['phoneNumber'],
+
+            });
+
+            ResponseUtils.sendGenericResponse(res);
+
+        }
+        catch (e) {
+            ResponseUtils.sendServerError(res)
+        }
+    }
+
 
 }
--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,10 @@ router.get('/', (req, res)=> res.send('RedeemFundApi v1.0'))
 //Authentication Routes
 router.post('/login',AuthController.login);
 router.post('/register',AuthController.register);
+router.post('/password-reset',AuthController.sendPasswordReset);
+router.get('/validate-password/:token',AuthController.validatePasswordResetToken);
+
+router.post('/set-new-password/:token',AuthController.setNewPassword);
 
 
 //Routes beneath this require authentication
@@ -23,6 +27,7 @@ router.route('/campaigns')
 
 
 //User Routes
+router.patch('/beneficiary', UserController.createProfile);
 router.get('/beneficiary/:id', UserController.getProfile);
 
 module.exports = router;
