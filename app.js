68a7daccc416d933881c6386663cd91da6667a59
Emmah Kimari <emmah.kimari@awamuplc.com>
2021-10-04 11:35:31 +0300

    add swagger documentation

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const app = express();
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerDefinition = require('./swagger.json');
 
 const Routes = require('./routes/api');
 
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js'],
+}
 
+const specs = swaggerJSDoc(options);
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
@@ -21,7 +29,6 @@ mongoose.connect(process.env.MONGODB_URI)
         process.exit(0);
     });
 
-
 app.use(logger('dev'))
 
 app.use(cors());
@@ -31,6 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api', Routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/webhook', require('./routes/webhooks'));
 
 app.listen(process.env.PORT, () => {
